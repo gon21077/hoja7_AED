8ab14f97b5a8cc2be992acaee67a50283dbd7f6a
Sebastian Franco <87022337+sebastianf232@users.noreply.github.com>
2022-03-28 11:51:17 -0600

    Documentacion

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,18 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BinarySearchTree {
+/**
+ * Algoritmos y Estructura de Datos
+ * Universidad del Valle de Guatemala
+ * Hoja de Trabajo No.7
+ * File: BinarySearchTree
+ * @author Sebastián Franco - 21484
+ * @author Juan Miguel Gonzalez-Campo - 21077
+ * @version 1.1
+ * @since 28/03/2021
+ */
+
+public class BinarySearchTree { 
     public TreeNode root;
 
-    BinarySearchTree(diccionario rootValue) {
+    /**
+     * Constructor de la clase
+     * @param rootValue
+     */
+    BinarySearchTree(diccionario rootValue) { 
         root = new TreeNode(rootValue);
     }
 
-    BinarySearchTree() {
+    BinarySearchTree() { ///Se crea 
         root = null;
     }
 
-    public boolean insert(diccionario value) {
+    /**
+     * @param value
+     * @return boolean
+     */
+    public boolean insert(diccionario value) {  ///Se inserta en el diccionario
         if (root == null) {
             root = new TreeNode(value);
             return true;
@@ -23,21 +42,28 @@ public class BinarySearchTree {
 
 //    ********************************* Breadth first search ***************************************
 
-    public List<diccionario> BreadthFirstSearch() {
+    /**
+     * @return BreadthFirstSearch
+     */
+    public List<diccionario> BreadthFirstSearch() { 
         return BreadthFirstSearch(root);
     }
 
-    public List<diccionario> BreadthFirstSearch(TreeNode root) {
+    /**
+     * @param root
+     * @return diccionario 
+     */
+    public List<diccionario> BreadthFirstSearch(TreeNode root) { ///Se crea el Arbol binario
         ArrayList<diccionario> bfs_list = new ArrayList<diccionario>();
         ArrayList<TreeNode> q = new ArrayList<TreeNode>();
         q.add(root);
         while (q.size() > 0) {
             TreeNode currNode = q.remove(q.size() - 1);
             bfs_list.add(currNode.dic);
-            if (currNode.left != null) {
+            if (currNode.left != null) { ///Se aniade la rama izquierda
                 q.add(0, currNode.left);
             }
-            if (currNode.right != null) {
+            if (currNode.right != null) { ///Se aniade la rama derecha
                 q.add(0, currNode.right);
             }
         }
@@ -47,23 +73,39 @@ public class BinarySearchTree {
 
     //  ******************** Depth first search inorder,preorder,postorder **********************
 
-    public List<diccionario> depthFirstSearch_InOrder() {
+    /**
+     * @return InOrder
+     */
+    public List<diccionario> depthFirstSearch_InOrder() { ///Buscar en odrden
         ArrayList<diccionario> dps_lst = new ArrayList<diccionario>();
         return traverseInOrder(root, dps_lst);
     }
 
-    public List<diccionario> traverseInOrder(TreeNode root, List<diccionario> dps_lst) {
+    /**
+     * @param root
+     * @param dps_lst
+     * @return dps_list
+     */
+    public List<diccionario> traverseInOrder(TreeNode root, List<diccionario> dps_lst) { 
         if (root.left != null) traverseInOrder(root.left, dps_lst);
-        dps_lst.add(root.dic);
+            dps_lst.add(root.dic);
         if (root.right != null) traverseInOrder(root.right, dps_lst);
-        return dps_lst;
+            return dps_lst;
     }
 
-    public List<diccionario> depthFirstSearch_PreOrder() {
+    /**
+     * @return transversePreOrder
+     */
+    public List<diccionario> depthFirstSearch_PreOrder() { 
         ArrayList<diccionario> dps_lst = new ArrayList<diccionario>();
         return traversePreOrder(root, dps_lst);
     }
 
+    /**
+     * @param root
+     * @param dps_lst
+     * @return dps_list
+     */
     public List<diccionario> traversePreOrder(TreeNode root, List<diccionario> dps_lst) {
         dps_lst.add(root.dic);
         if (root.left != null) traversePreOrder(root.left, dps_lst);
@@ -71,11 +113,16 @@ public class BinarySearchTree {
         return dps_lst;
     }
 
-    public List<diccionario> depthFirstSearch_PostOrder() {
+    public List<diccionario> depthFirstSearch_PostOrder() { ///Busca el primer elemento
         ArrayList<diccionario> dps_lst = new ArrayList<diccionario>();
         return traversePostOrder(root, dps_lst);
     }
 
+    /**
+     * @param root
+     * @param dps_lst
+     * @return
+     */
     public List<diccionario> traversePostOrder(TreeNode root, List<diccionario> dps_lst) {
         if (root.left != null) traversePostOrder(root.left, dps_lst);
         if (root.right != null) traversePostOrder(root.right, dps_lst);
@@ -110,12 +157,22 @@ public class BinarySearchTree {
 
     //    ******************* delete a node from tree **********************
 
+    /**
+     * @param value
+     * @return Parent Node
+     */
     public List<TreeNode> findNodeAlsoItsParent(diccionario value) {
         TreeNode parent = null;
         return findNodeAndItsParent(parent, root, value);
     }
 
-    public List<TreeNode> findNodeAndItsParent(TreeNode parent, TreeNode root, diccionario value) {
+    /**
+     * @param parent
+     * @param root
+     * @param value
+     * @return list
+     */
+    public List<TreeNode> findNodeAndItsParent(TreeNode parent, TreeNode root, diccionario value) { //Encuentra la Raiz
         ArrayList<TreeNode> lst = new ArrayList<TreeNode>();
         if (value.compareTo(root.dic) == 0) {
             lst.add(parent);
@@ -137,7 +194,10 @@ public class BinarySearchTree {
         }
     }
 
-    public void delete(diccionario value) {
+    /**
+     * @param value
+     */
+    public void delete(diccionario value) { //Elimina del diccionario
         List<TreeNode> lst = findNodeAlsoItsParent(value);
         TreeNode nodeToBeDeleted = lst.get(1);
         TreeNode parentNode = lst.get(0);
@@ -161,7 +221,8 @@ public class BinarySearchTree {
 
 
     //  ***************************** TreeNode ****************************************
-    public static class TreeNode {
+
+    public static class TreeNode { ///Treenode.
         public diccionario dic;
         public TreeNode left;
         public TreeNode right;
--- diccionario.java
+++ diccionario.java
@@ -5,22 +5,31 @@ public class diccionario implements Comparable<diccionario> {
     private HashMap<String, String> palabras;
     private String ingles;
     
-    
-    public diccionario(String leng1, HashMap<String, String> palab){
-        this.ingles = leng1;
+    /**
+     * @param leng1
+     * @param palab
+     */
+    public diccionario(String leng1, HashMap<String, String> palab){ ///Diccionario
+        this.ingles = leng1; 
         this.palabras = palab;
     }
 
+    /**
+     * @return ingles
+     */
     public String getName(){
         return this.ingles;
     }
-    public HashMap<String, String> getHashMap(){
+    /**
+     * @return HashMap Palabras
+     */
+    public HashMap<String, String> getHashMap(){ /// HashMap Palabras
         return this.palabras;
     }
 
 
     @Override
-    public int compareTo(diccionario other) {
+    public int compareTo(diccionario other) { ///Compara las palabras.
         return this.ingles.compareTo(other.ingles);
     }
 }
